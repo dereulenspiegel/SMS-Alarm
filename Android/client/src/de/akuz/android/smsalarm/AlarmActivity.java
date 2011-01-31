315b191d43e1f51e9c88f19002ccdf133b0a040e
Till Klocke <github@goesberserk.de>
2011-01-31 17:25:26 +0100

    The AlarmActivity is now able to play a notification sound and to set the notification volume to maximum and reset the volume after the alarm is acknowlegded

--- Android/client/src/de/akuz/android/smsalarm/AlarmActivity.java
+++ Android/client/src/de/akuz/android/smsalarm/AlarmActivity.java
@@ -1,8 +1,14 @@
 package de.akuz.android.smsalarm;
 
 import de.akuz.android.smsalarm.data.Alarm;
+import de.akuz.android.smsalarm.util.TextUtils;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -12,6 +18,15 @@ public class AlarmActivity extends Activity {
 	private AlarmListAdapter alarmListAdapter;
 	private Intent callingIntent;
 	
+	private String ringtoneUri;
+	private int ledColor=-1;
+	
+	private boolean continueNotification = false;
+	
+	private AudioManager am;
+	
+	private int oldNotificationVolume;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +35,7 @@ public class AlarmActivity extends Activity {
 		alarmListAdapter = new AlarmListAdapter(this);
 		alarmListView.setAdapter(alarmListAdapter);
 		callingIntent = this.getIntent();
+		am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
 	}
 
 	@Override
@@ -29,10 +45,57 @@ public class AlarmActivity extends Activity {
 		Alarm[] alarms = 
 			(Alarm[]) callingIntent.getParcelableArrayExtra(Alarm.PARCELABLE_KEYWORD);
 		alarmListAdapter.clear();
+		
 		for(int i=0;i<alarms.length;i++){
+			//Set the notification settings. We use the first available settings to+
+			//notify the user
+			if(!TextUtils.isNonEmptyString(ringtoneUri) && 
+					TextUtils.isNonEmptyString(alarms[i].getRingtoneUri())){
+				ringtoneUri = alarms[i].getRingtoneUri();
+			}
+			if(ledColor == -1 && alarms[i].getLedColor()>-1){
+				ledColor = alarms[i].getLedColor();
+			}
 			alarmListAdapter.add(alarms[i]);
 		}
-		//TODO: Sound the alarm
+		if(alarms.length > 0){
+			continueNotification = true;
+			oldNotificationVolume = am.getStreamVolume(AudioManager.STREAM_NOTIFICATION);
+			am.setStreamVolume(AudioManager.STREAM_NOTIFICATION, 
+					am.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION), 0);
+			startNotification();
+		}
+	}
+	
+	/**
+	 * This method starts the whole notification. Plays the ringtone, lets the LED blink
+	 * and so on
+	 */
+	private void startNotification(){
+		final Ringtone alert = RingtoneManager.getRingtone(this, Uri.parse(ringtoneUri));
+		new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				while(continueNotification){
+					if(!alert.isPlaying()){
+						alert.play();
+					}
+				}
+				alert.stop();
+				
+			}
+			
+		}).start();
+		
+	}
+	
+	/**
+	 * This stops the alarm and resets the notification volume
+	 */
+	private void stopAlarm(){
+		continueNotification = false;
+		am.setStreamVolume(AudioManager.STREAM_NOTIFICATION, oldNotificationVolume, 0);
 	}
 
 	@Override
