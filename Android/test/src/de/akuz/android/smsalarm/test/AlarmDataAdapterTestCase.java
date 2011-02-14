343985163fb1ac5b31a877a0e12baceeeec7e5aa
Till Klocke <github@goesberserk.de>
2011-02-14 10:31:15 +0100

    Modified the tests for AlarmDataAdapter so they should work now

--- Android/test/src/de/akuz/android/smsalarm/test/AlarmDataAdapterTestCase.java
+++ Android/test/src/de/akuz/android/smsalarm/test/AlarmDataAdapterTestCase.java
@@ -6,6 +6,7 @@ import de.akuz.android.smsalarm.data.AlarmDataAdapter;
 import de.akuz.android.smsalarm.data.AlarmGroup;
 import android.content.Context;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 public class AlarmDataAdapterTestCase extends AndroidTestCase {
 	
@@ -16,7 +17,7 @@ public class AlarmDataAdapterTestCase extends AndroidTestCase {
 	private final static String TEST_KEYWORD="bhp1";
 	private final static String TEST_SENDER="juh_do";
 	
-	AlarmGroup group;
+	private final static String TAG = "AlarmDataAdapterTestCase";
 
 	@Override
 	protected void setUp() throws Exception {
@@ -39,19 +40,21 @@ public class AlarmDataAdapterTestCase extends AndroidTestCase {
 		Assert.assertEquals(newReference, testAdapter);
 	}
 
-	public void testCreatingNewAlarmGroup() throws Exception {
-		group = testAdapter.createNewAlarmGroup(TEST_NAME, TEST_KEYWORD);
+	public void testCreatingNewAlarmGroupAndAddingAllowedNumber() throws Exception {
+		AlarmGroup group = testAdapter.createNewAlarmGroup(TEST_NAME, TEST_KEYWORD);
 		Assert.assertEquals(1,testAdapter.getAllAlarmGroups().size());
-	}
-	
-	public void testListOfAllowedNumbers() throws Exception { 
+		Log.d(TAG, "Adding new allowed number to group");
 		group.addAllowedNumber(TEST_SENDER);
+		Log.d(TAG,"Checking if list if allowed numbers has length of one");
 		Assert.assertEquals(1, group.getAllowedNumbers().size());
+		Log.d(TAG,"Checking if the allowed number equals our number");
 		Assert.assertEquals(TEST_SENDER, group.getAllowedNumbers().get(0));
 	}
 	
 	public void testRetrieveGroupByNumberAndKeyword() throws Exception {
-		AlarmGroup group = 
+		AlarmGroup group = testAdapter.createNewAlarmGroup(TEST_NAME, TEST_KEYWORD);
+		group.addAllowedNumber(TEST_SENDER);
+		group = 
 			testAdapter.getAlarmGroupByNumberAndKeyword(TEST_SENDER, TEST_KEYWORD);
 		Assert.assertNotNull(group);
 		Assert.assertEquals(TEST_KEYWORD, group.getKeyword());
