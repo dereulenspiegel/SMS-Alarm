f7d588d55ae6fc27974974461f442e34a0c90450
Till Klocke <github@goesberserk.de>
2011-02-15 18:08:00 +0100

    Optimized the method isNonEmptyString in TextUtils

--- Android/client/src/de/akuz/android/smsalarm/util/TextUtils.java
+++ Android/client/src/de/akuz/android/smsalarm/util/TextUtils.java
@@ -2,11 +2,16 @@ package de.akuz.android.smsalarm.util;
 
 public class TextUtils {
 	
-	public static boolean isNonEmptyString(String in){
-		if(in == null || in.trim().length() == 0){
+	public static boolean isNonEmptyString(final String in){
+		if(in == null){
 			return false;
 		}
-		return true;
+		for(char c : in.toCharArray()){
+			if(!Character.isWhitespace(c)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
