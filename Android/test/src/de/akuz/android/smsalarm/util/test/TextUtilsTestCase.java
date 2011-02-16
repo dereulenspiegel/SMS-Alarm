a5e56346e65dd5c0f35cbb0de8353183f769eefd
Till Klocke <github@goesberserk.de>
2011-02-16 11:55:12 +0100

    Fixed some small issues in TextUtilsTestCase

--- Android/test/src/de/akuz/android/smsalarm/util/test/TextUtilsTestCase.java
+++ Android/test/src/de/akuz/android/smsalarm/util/test/TextUtilsTestCase.java
@@ -8,22 +8,22 @@ public class TextUtilsTestCase extends TestCase {
 	
 	public void testForNonEmptyString() throws Exception {
 		String testString = "Non empty String";
-		Assert.assertEquals(true, TextUtils.isNonEmptyString(testString));
+		Assert.assertTrue(TextUtils.isNonEmptyString(testString));
 	}
 	
 	public void testForEmptyString() throws Exception {
 		String testString = "";
-		Assert.assertEquals(false, TextUtils.isNonEmptyString(testString));
+		Assert.assertFalse(TextUtils.isNonEmptyString(testString));
 	}
 	
 	public void testForNull() throws Exception {
 		String testString = null;
-		Assert.assertEquals(false, TextUtils.isNonEmptyString(testString));
+		Assert.assertFalse(TextUtils.isNonEmptyString(testString));
 	}
 	
 	public void testForSingleSpace() throws Exception {
 		String testString = " ";
-		Assert.assertEquals(false, TextUtils.isNonEmptyString(testString));
+		Assert.assertFalse(TextUtils.isNonEmptyString(testString));
 	}
 
 }
