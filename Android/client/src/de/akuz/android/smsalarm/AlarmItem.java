6420c7e2e008bb1c9fbd84d40613d77a464626c5
Till Klocke <github@goesberserk.de>
2011-02-17 11:30:37 +0100

    Fixed some PMD complaints in AlarmItem

--- Android/client/src/de/akuz/android/smsalarm/AlarmItem.java
+++ Android/client/src/de/akuz/android/smsalarm/AlarmItem.java
@@ -22,10 +22,12 @@ public class AlarmItem extends LinearLayout {
 	
 	private Context mContext;
 
-	public AlarmItem(Context context, String message, String description) {
+	public AlarmItem(final Context context, final String message, 
+			final String description) {
 		super(context);
 		this.mContext = context;
-		View view = LayoutInflater.from(context).inflate(R.layout.alarm_item, null);
+		final View view = 
+			LayoutInflater.from(context).inflate(R.layout.alarm_item, null);
 		this.addView(view);
 		this.date = (TextView)view.findViewById(R.id.TextViewDate);
 		this.description = (TextView)view.findViewById(R.id.TextViewDescription);
@@ -35,15 +37,15 @@ public class AlarmItem extends LinearLayout {
 		this.description.setText(description);
 	}
 
-	public void setDate(Date date) {
+	public void setDate(final Date date) {
 		this.date.setText(DateFormat.getDateFormat(mContext).format(date));
 	}
 
-	public void setDescription(String description) {
+	public void setDescription(final String description) {
 		this.description.setText(description);
 	}
 
-	public void setMessage(String message) {
+	public void setMessage(final String message) {
 		this.message.setText(message);
 	}
 
